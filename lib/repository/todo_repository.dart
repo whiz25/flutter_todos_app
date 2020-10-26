a69b0a344ccec4ad3e1904e0e324f2d73e31bdf9
Martin Afani <martin@orderly.ai>
2020-10-26 19:32:09 +0300

    Use todo's id to update, delete and create

--- lib/repository/todo_repository.dart
+++ lib/repository/todo_repository.dart
@@ -17,72 +17,71 @@ class TodoRepository implements ITodoRepository {
   }
 
   @override
-  Future<Todo> addTodo(TodoList todoList, Todo todo) async {    
+  Future<Todo> addTodo(TodoList todoList, Todo todo) async {
     final todoBox = await getBoxForList(todoList);
-    await todoBox.add(todo);    
+    await todoBox.put(todo.id, todo);
     return todo;
   }
-    
-      @override
-      Future<void> deleteTodo(TodoList todoList, Todo todo) async {        
-        final todoBox = await getBoxForList(todoList);
-        await todoBox.delete(todo.id);
-      }
-    
-      @override
-      Future<void> deleteTodoList(TodoList todoList) async {        
-        await checkIfTodoListsBoxIsCreatedAndOpen();
-        await _todoListBox.delete(todoList.key);
 
-        final todoBox = await getBoxForList(todoList);
-        await todoBox.deleteFromDisk();        
-      }
-    
-      @override
-      Future<List<Todo>> getAllTodos(TodoList todoList) async {
-        final todoBox = await getBoxForList(todoList);        
-        return todoBox.values.cast<Todo>().toList();    
-      }
-    
-      @override
-      Future<Todo> updateTodo(TodoList todoList, Todo todo) async {
-        final todoBox = await getBoxForList(todoList);            
-        await todoBox.put(todo.id, todo);    
-        return todo;
-      }     
-    
-      @override
-      Future<TodoList> addTodoList(TodoList todoList) async {
-        await checkIfTodoListsBoxIsCreatedAndOpen();
-    
-        await _todoListBox.add(todoList);
-    
-        return todoList;
-      }
-    
-      @override
-      Future<List<TodoList>> getAllTodoLists() async {
-        await checkIfTodoListsBoxIsCreatedAndOpen();
-    
-        final Iterable<TodoList> todoLists = 
-          _todoListBox.values.cast<TodoList>();
-        return todoLists.toList();
-      }
+  @override
+  Future<void> deleteTodo(TodoList todoList, Todo todo) async {
+    final todoBox = await getBoxForList(todoList);
+    await todoBox.delete(todo.id);
+  }
 
-      @override
-      Future deleteAllTodos(TodoList todoList) async {
-        final todoBox = await getBoxForList(todoList);
-        await todoBox.clear();
-      }
-    
-      Future<void> checkIfTodoListsBoxIsCreatedAndOpen() async {
-        _todoListBox ??= await Hive.openBox<TodoList>('allTodoLists');
-    
-        if (!(_todoListBox?.isOpen ?? false)) {
-          return;
-        }
-      }
-            
-      Future<Box> getBoxForList(TodoList todoList) =>
-        Hive.openBox<Todo>(todoList.id);
+  @override
+  Future<void> deleteTodoList(TodoList todoList) async {
+    await checkIfTodoListsBoxIsCreatedAndOpen();
+    await _todoListBox.delete(todoList.key);
+
+    final todoBox = await getBoxForList(todoList);
+    await todoBox.deleteFromDisk();
+  }
+
+  @override
+  Future<List<Todo>> getAllTodos(TodoList todoList) async {
+    final todoBox = await getBoxForList(todoList);
+    return todoBox.values.cast<Todo>().toList();
+  }
+
+  @override
+  Future<Todo> updateTodo(TodoList todoList, Todo todo) async {
+    final todoBox = await getBoxForList(todoList);
+    await todoBox.put(todo.id, todo);
+    return todo;
+  }
+
+  @override
+  Future<TodoList> addTodoList(TodoList todoList) async {
+    await checkIfTodoListsBoxIsCreatedAndOpen();
+
+    await _todoListBox.add(todoList);
+
+    return todoList;
+  }
+
+  @override
+  Future<List<TodoList>> getAllTodoLists() async {
+    await checkIfTodoListsBoxIsCreatedAndOpen();
+
+    final Iterable<TodoList> todoLists = _todoListBox.values.cast<TodoList>();
+    return todoLists.toList();
+  }
+
+  @override
+  Future deleteAllTodos(TodoList todoList) async {
+    final todoBox = await getBoxForList(todoList);
+    await todoBox.clear();
+  }
+
+  Future<void> checkIfTodoListsBoxIsCreatedAndOpen() async {
+    _todoListBox ??= await Hive.openBox<TodoList>('allTodoLists');
+
+    if (!(_todoListBox?.isOpen ?? false)) {
+      return;
+    }
+  }
+
+  Future<Box> getBoxForList(TodoList todoList) =>
+      Hive.openBox<Todo>(todoList.id);
 }
